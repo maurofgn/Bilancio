6d8327c89338df535fbe7c608c621e6642e85469
Mauro Fugante <mauro.fugante@gmail.com>
2015-10-13 17:28:44 +0200

    v2

---
+++ contrib/SQLQuery1.sql
@@ -0,0 +1,22 @@
+﻿select *
+from AccountCee
+--where parentID is null
+where NodeType = 7
+and summary = 1
+order by code
+
+select c.*
+,p.code padre_code, p.name padre_name
+from AccountCee c
+left join AccountCee p on p.ID = c.ParentID
+where c.id = (select max(ID) from AccountCee)
+
+
+
+select c.active
+,c.code, c.name 
+from AccountCee c
+where active = 0
+
+select *
+from AccountChart
