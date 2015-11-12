e912bfd1a186992950125b8523a4dd4111f4f801
Mauro Fugante <mauro.fugante@gmail.com>
2015-11-12 11:23:06 +0100

    query docs

--- contrib/SQLQuery1.sql
+++ contrib/SQLQuery1.sql
@@ -1,22 +1,11 @@
-﻿select *
-from AccountCee
---where parentID is null
-where NodeType = 7
-and summary = 1
-order by code
-
-select c.*
-,p.code padre_code, p.name padre_name
-from AccountCee c
-left join AccountCee p on p.ID = c.ParentID
-where c.id = (select max(ID) from AccountCee)
-
-
-
-select c.active
-,c.code, c.name 
-from AccountCee c
-where active = 0
-
-select *
-from AccountChart
+﻿select
+dt.name type,
+h.docNr,h.dateReg,
+c.code, c.name,
+ r.*
+from AccountChart c
+inner join DocumentRow r on r.AccountChart_ID = c.ID
+inner join Document h on h.id = r.Document_ID
+inner join DocumentType dt on dt.ID = h.DocumentType_ID
+where c.Code = 'T.11.1'	--T.11.1 	Rimborsi per donazioni 	-20,00 	0,00 	0,00
+and year(h.dateReg) = 2015
