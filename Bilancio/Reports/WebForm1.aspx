4e91f4bfb9d5af04de7ee6cb1d77695c51a7b43d
Mauro Fugante <mauro.fugante@gmail.com>
2015-11-12 11:41:30 +0100

    tentativo di utilizzo del tool report. Il tentativo non è riuscito

---
+++ Bilancio/Reports/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="Bilancio.WebForm1" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+    
+    </div>
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Font-Names="Verdana" Font-Size="8pt" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt">
+            <LocalReport ReportPath="Reports\Balance.rdlc">
+            </LocalReport>
+        </rsweb:ReportViewer>
+    </form>
+</body>
+</html>
---
+++ Bilancio/Reports/WebForm1.aspx.designer.vb
@@ -0,0 +1,42 @@
+﻿'------------------------------------------------------------------------------
+' <generato automaticamente>
+'     Codice generato da uno strumento.
+'
+'     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+'     il codice viene rigenerato. 
+' </generato automaticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class WebForm1
+
+    '''<summary>
+    '''Controllo form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generato automaticamente.
+    '''Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Controllo ScriptManager1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generato automaticamente.
+    '''Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+    '''</remarks>
+    Protected WithEvents ScriptManager1 As Global.System.Web.UI.ScriptManager
+
+    '''<summary>
+    '''Controllo ReportViewer1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generato automaticamente.
+    '''Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+    '''</remarks>
+    Protected WithEvents ReportViewer1 As Global.Microsoft.Reporting.WebForms.ReportViewer
+End Class
